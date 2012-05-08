vanicat/afew

commit b58ac76d5723146165fbcd3d0a3396d71e123c16

Ignore encoding errors when parsing mails with Python 3.1
Python 3.1 doesn't have email.message_from_binary_file(), so when running
under this version of Python we need to use the old email.message_from_file()
function that takes an (already decoded) string rather than raw bytes. As we
used the built-in open() method (in text mode) to provide input to
message_from_file(), all errors were handled strictly, causing afew to abort
with a Traceback when it encountered a message that was encoded in a character
set different from the current locale.

Work around this by silently ignoring encoding errors. This means all
non-ASCII will be filtered out on many messages (which affects tagging), but
it's still better than breaking.

The real fix is to update Python to 3.2, but unfortunately Debian Squeeze
(stable) ships 3.1.3 only.

Signed-off-by: Sascha Silbe <sascha-pgp@silbe.org>

afew/utils.py
@@ -88,7 +88,8 @@ def extract_mail_body(message):
     if hasattr(email, 'message_from_binary_file'):
         mail = email.message_from_binary_file(open(message.get_filename(), 'br'))
     else:
-        mail = email.message_from_file(open(message.get_filename()))
+        fp = codecs.open(message.get_filename(), 'r', 'utf-8', errors='ignore')
+        mail = email.message_from_file(fp)
 
     content = []
     for part in mail.walk():

commit 1d3464906673e48aa7eea89a6b7209f27c4dc523

Robustness fix: replace invalid characters when decoding messages
Fixes a regression introduced by b58ac76 when using python < 3.2.

afew/utils.py
@@ -88,7 +88,7 @@ def extract_mail_body(message):
     if hasattr(email, 'message_from_binary_file'):
         mail = email.message_from_binary_file(open(message.get_filename(), 'br'))
     else:
-        fp = codecs.open(message.get_filename(), 'r', 'utf-8', errors='ignore')
+        fp = codecs.open(message.get_filename(), 'r', 'utf-8', errors='replace')
         mail = email.message_from_file(fp)
 
     content = []